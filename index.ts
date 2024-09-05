ec4e8ea83b291be485e7cf44eb48cf6046c6a0b2
estrela801 <rohenri0808@gmail.com>
2024-09-05 18:28:07 -0300

    feat: creating the routes for the application

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-const express:any = require('express')
+const express= require('express')
+const app = express()
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'public')))
+function Root(page: any){
+    return path.join(__dirname, `public/${page}`)
+}
+
+app.get('/', (req: any,res: any)=>{
+    res.sendFile(Root('index.html'))
+})
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT)
